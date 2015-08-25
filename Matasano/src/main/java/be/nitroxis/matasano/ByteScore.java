08b044718bcb308892dd2c0215f7e45aad80280a
Olivier Houyoux (a590568) <olivier@OlivierLinux>
2015-08-25 10:53:58 +0200

    Make the scoring mechanism open

---
+++ Matasano/src/main/java/be/nitroxis/matasano/ByteScore.java
@@ -0,0 +1,31 @@
+package be.nitroxis.matasano;
+
+/**
+ * Scores a piece of data trying to .
+ *
+ * @author Olivier Houyoux
+ */
+public class ByteScore implements Score<byte[]> {
+
+    @Override
+    public int getScore(final byte[] data) {
+        int score = 0;
+
+        for (int i = 0; i < data.length; i++) {
+            boolean isEnglish = isEnglish(data[i]);
+
+            if (isEnglish) {
+                score++;
+            }
+        }
+
+        return score;
+    }
+
+    private boolean isEnglish(final byte c) {
+        return (c >= '0' && c <= '9')
+               || (c >= 'a' && c <= 'z')
+               || (c >= 'A' && c <= 'Z')
+               || c == ' ';
+    }
+}
