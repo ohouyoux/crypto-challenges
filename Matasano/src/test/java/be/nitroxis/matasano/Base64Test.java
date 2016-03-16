8742931efa33b1cf62e9ccf4ad82f7e4148af2e7
Olivier Houyoux <olivier.houyoux@gmail.com>
2016-03-17 00:59:33 +0100

    Fix line length issue

--- Matasano/src/test/java/be/nitroxis/matasano/Base64Test.java
+++ Matasano/src/test/java/be/nitroxis/matasano/Base64Test.java
@@ -5,13 +5,13 @@ import org.junit.Test;
 
 public class Base64Test {
 
-    private final Base64 systemUnderTest = new Base64();
-
     @Test
     public void shouldConvertHexToBase64() {
-        String hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
-        String result = systemUnderTest.toHex(hex);
+        String hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f757"
+          + "3206d757368726f6f6d";
+        String result = Base64.hexToBase64(hex);
 
-        assertThat(result).isEqualTo("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t");
+        assertThat(result)
+          .isEqualTo("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t");
     }
 }
