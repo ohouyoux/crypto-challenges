4b9b13eae8003811e9eecf022a3f2a60da9480bd
Olivier Houyoux <olivier.houyoux@gmail.com>
2016-03-17 12:00:59 +0100

    Implement Hex as a utility class

--- Matasano/src/test/java/be/nitroxis/matasano/XorTest.java
+++ Matasano/src/test/java/be/nitroxis/matasano/XorTest.java
@@ -9,10 +9,9 @@ public class XorTest {
 
     @Test
     public void shouldApplyExclusiveDisjunction() {
-        Hex hex = new Hex();
-        byte[] data1 = hex.decode("1c0111001f010100061a024b53535009181c".toCharArray());
-        byte[] data2 = hex.decode("686974207468652062756c6c277320657965".toCharArray());
-        char[] result = hex.encode(systemUnderTest.apply(data1, data2));
+        byte[] data1 = Hex.decode("1c0111001f010100061a024b53535009181c".toCharArray());
+        byte[] data2 = Hex.decode("686974207468652062756c6c277320657965".toCharArray());
+        char[] result = Hex.encode(systemUnderTest.apply(data1, data2));
 
         assertThat(new String(result)).isEqualTo("746865206b696420646f6e277420706c6179");
     }
