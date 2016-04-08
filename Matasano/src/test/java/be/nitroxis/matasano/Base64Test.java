cbde6e4fd9724bf1e2ba7d522debf3e7eb756c62
Olivier Houyoux <olivier.houyoux@gmail.com>
2016-04-08 22:04:11 +0200

    Fix typo

--- Matasano/src/test/java/be/nitroxis/matasano/Base64Test.java
+++ Matasano/src/test/java/be/nitroxis/matasano/Base64Test.java
@@ -7,11 +7,11 @@ public class Base64Test {
 
     @Test
     public void shouldConvertHexToBase64() {
-        String hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f757"
-          + "3206d757368726f6f6d";
-        String result = Base64.hexToBase64(hex);
+      String hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f757"
+        + "3206d757368726f6f6d";
+      String result = Base64.hexToBase64(hex);
 
-        assertThat(result)
+      assertThat(result)
           .isEqualTo("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t");
     }
 }
